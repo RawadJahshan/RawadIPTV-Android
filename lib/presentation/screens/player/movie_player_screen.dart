1215ff4364ded15bf54955b22e74b576abc24852
Rawad Jahshan <70718218+RawadJahshan@users.noreply.github.com>
2026-03-28 23:07:04 +0300

    Force fullscreen movie player with close overlay

--- lib/presentation/screens/player/movie_player_screen.dart
+++ lib/presentation/screens/player/movie_player_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tha_player/tha_player.dart';
 
@@ -39,9 +40,7 @@ class _MoviePlayerScreenState extends State<MoviePlayerScreen> {
   Duration _duration = Duration.zero;
   DateTime? _lastProgressSaveAt;
 
-  @override
-  void initState() {
-    super.initState();
+  void _initPlayer() {
     _ctrl = ThaNativePlayerController.single(
       ThaMediaSource(
         widget.streamUrl,
@@ -58,6 +57,19 @@ class _MoviePlayerScreenState extends State<MoviePlayerScreen> {
         _ctrl.seekTo(widget.startAt!);
       });
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.immersiveSticky,
+    );
+    _initPlayer();
 
     _progressTimer =
         Timer.periodic(const Duration(seconds: 5), (_) => _saveProgress());
@@ -100,6 +112,11 @@ class _MoviePlayerScreenState extends State<MoviePlayerScreen> {
 
   @override
   void dispose() {
+    SystemChrome.setPreferredOrientations(DeviceOrientation.values);
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.manual,
+      overlays: SystemUiOverlay.values,
+    );
     _progressTimer?.cancel();
     unawaited(_saveProgress(force: true));
     _ctrl.dispose();
@@ -108,6 +125,10 @@ class _MoviePlayerScreenState extends State<MoviePlayerScreen> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.immersiveSticky,
+    );
+
     return PopScope(
       canPop: true,
       onPopInvokedWithResult: (didPop, result) {
@@ -124,38 +145,50 @@ class _MoviePlayerScreenState extends State<MoviePlayerScreen> {
               doubleTapSeek: const Duration(seconds: 10),
               autoHideAfter: const Duration(seconds: 3),
               initialBoxFit: BoxFit.contain,
-            ),
-            Positioned(
-              top: 8,
-              left: 8,
-              right: 60,
-              child: Text(
-                widget.title,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 15,
-                  fontWeight: FontWeight.w600,
-                  shadows: [
-                    Shadow(
-                      color: Colors.black,
-                      blurRadius: 4,
+              isFullscreen: true,
+              overlay: Stack(
+                children: [
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 60,
+                    child: SafeArea(
+                      child: Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: Text(
+                          widget.title,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                            shadows: [
+                              Shadow(
+                                color: Colors.black54,
+                                blurRadius: 4,
+                              ),
+                            ],
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
                     ),
-                  ],
-                ),
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-            Positioned(
-              top: 8,
-              right: 8,
-              child: IconButton(
-                icon: const Icon(
-                  Icons.close,
-                  color: Colors.white,
-                  size: 28,
-                ),
-                onPressed: () => Navigator.of(context).pop(),
+                  ),
+                  Positioned(
+                    top: 0,
+                    right: 0,
+                    child: SafeArea(
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.close,
+                          color: Colors.white,
+                          size: 28,
+                        ),
+                        onPressed: () => Navigator.of(context).pop(),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
             ValueListenableBuilder<ThaPlaybackState>(
